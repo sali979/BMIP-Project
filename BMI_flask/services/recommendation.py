8fd2190878cfdaf836c883f56a064f3c560a55cd
Syed Ali <swali1@buffs.wtamu.edu>
2022-04-05 02:24:15 -0500

    Website and flask are working
    need to create more tests in "tests"

--- BMI_flask/services/recommendation.py
+++ BMI_flask/services/recommendation.py
@@ -1,18 +1,18 @@
 def give_recommendation(score,target):
-    # if score < 16:
-    #     return "Get onto eating proteins and doing simple workouts to get your body into shape"
-    # elif 16 <= score < 18.5:
-    #     return "You need to eat more proteins and give your body more exercise"
-    # elif 18.5 <= score < 25:
-    #     return "Good work in watching your health"
-    # elif 25 <= score < 30:
-    #     return "Reduce your protein levels and get into more exercise"
-    # elif score >= 30:
-    #     return "Lower your proteins consumption \n start a workout program \n consult a health personelle for advice"
+    if score < 16:
+        return "Get onto eating proteins and doing simple workouts to get your body into shape"
+    elif 16 <= score < 18.5:
+        return "You need to eat more proteins and give your body more exercise"
+    elif 18.5 <= score < 25:
+        return "Good work in watching your health"
+    elif 25 <= score < 30:
+        return "Reduce your protein levels and get into more exercise"
+    elif score >= 30:
+        return "Lower your proteins consumption \n start a workout program \n consult a health personelle for advice"
 
-    if abs(score - target) >= 20:
-        return "Least"
-    elif abs(score - target) >= 10:
-        return "Good"
-    elif abs(score - target) < 5:
-        return "Ideal"
+    #if abs(score - target) >= 20:
+    #    return "Least"
+    #elif abs(score - target) >= 10:
+    #    return "Good"
+    #elif abs(score - target) < 5:
+    #    return "Ideal"
